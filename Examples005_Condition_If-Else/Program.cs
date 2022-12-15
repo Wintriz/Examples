2618d485684c19ecc94a4c44df9a4ad98334cb34
Andrey <rf5800@mail.ru>
2022-12-15 23:24:33 +0300

    Examples005_Condition_If-Else

---
+++ Examples005_Condition_If-Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Yay, its Masha!");   
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}

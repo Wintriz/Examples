63fa79411902ba8ff9526af20c1cab6fe839a7e0
Andrey <rf5800@mail.ru>
2022-12-20 18:52:59 +0300

    Examples12

---
+++ Examples12/Program.cs
@@ -0,0 +1,30 @@
+﻿// for(int i = 0; i < 10; i++)
+// {
+//  for(int j = 0; j < 10; j++)
+//  {
+//  Console.WriteLine(i*j)
+//  }
+//  Console.WriteLine()
+// }
+
+
+// Функции в программировании
+// void SelectionSort(int[] array)
+// {
+//  for (int i = 0; i < array.Length - 1; i++)
+//  {
+//  int minPosition = i;
+//  for (int j = i + 1; j < array.Length; j++)
+//  {
+//  if (array[j] < array[minPosition])
+//  {
+//  minPosition = j;
+//  }
+//  }
+//  int temporary = array[i];
+//  array[i] = array[minPosition];
+//  array[minPosition] = temporary;
+//  }
+// }
+
+
